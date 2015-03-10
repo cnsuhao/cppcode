7c063ab633a09890f13a9e0147e0572ac4f96c14
qq410029478 <qq410029478@ff87cf93-6e24-4a51-a1f1-68835326ac4f>
2015-03-10 03:57:36 +0000

    Lua 5.1.4

---
+++ OpenGL/InteractiveComputerGraphicsCode/GLFW/example1.cpp
@@ -0,0 +1,114 @@
+// Two-Dimensional Sierpinski Gasket       
+// Generated using randomly selected vertices and bisection
+
+#include "Angel.h"
+
+const int NumPoints = 5000;
+
+//----------------------------------------------------------------------------
+
+void
+init( void )
+{
+    vec2 points[NumPoints];
+
+    // Specifiy the vertices for a triangle
+    vec2 vertices[3] = {
+        vec2( -1.0, -1.0 ), vec2( 0.0, 1.0 ), vec2( 1.0, -1.0 )
+    };
+
+    // Select an arbitrary initial point inside of the triangle
+    points[0] = vec2( 0.25, 0.50 );
+
+    // compute and store N-1 new points
+    for ( int i = 1; i < NumPoints; ++i ) {
+        int j = rand() % 3;   // pick a vertex at random
+
+        // Compute the point halfway between the selected vertex
+        //   and the previous point
+        points[i] = ( points[i - 1] + vertices[j] ) / 2.0;
+    }
+
+    // Create a vertex array object
+    GLuint vao[1];
+    glGenVertexArrays( 1, vao );
+    glBindVertexArray( vao[0] );
+    
+
+    // Create and initialize a buffer object
+    GLuint buffer;
+    glGenBuffers( 1, &buffer );
+    glBindBuffer( GL_ARRAY_BUFFER, buffer );
+    glBufferData( GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW );
+
+    // Load shaders and use the resulting shader program
+    GLuint program = InitShader( "vshader21.glsl", "fshader21.glsl" );
+    glUseProgram( program );
+
+    // Initialize the vertex position attribute from the vertex shader
+    GLuint loc = glGetAttribLocation( program, "vPosition" );
+    glEnableVertexAttribArray( loc );
+    glVertexAttribPointer( loc, 2, GL_FLOAT, GL_FALSE, 0,
+                           BUFFER_OFFSET(0) );
+
+    glClearColor( 1.0, 1.0, 1.0, 1.0 ); // white background
+}
+
+//----------------------------------------------------------------------------
+
+void
+display( void )
+{
+    glClear( GL_COLOR_BUFFER_BIT );     // clear the window
+    glDrawArrays( GL_POINTS, 0, NumPoints );    // draw the points
+    glfwSwapBuffers();
+}
+
+
+//----------------------------------------------------------------------------
+
+void
+keyboard( unsigned char key, int x, int y )
+{
+    switch ( key ) {
+    case 033:
+        exit( EXIT_SUCCESS );
+        break;
+    }
+}
+
+//----------------------------------------------------------------------------
+
+int
+main( int argc, char **argv )
+{
+
+    // Initialise GLFW
+    glfwInit();
+
+    // Open a window and create its OpenGL context
+
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwOpenWindow( 500, 500, 0,0,0,0, 0,0, GLFW_WINDOW);
+
+    glfwSetWindowTitle( "Sierpinski Gasket" );
+
+    init();
+
+
+   do{
+      display();
+   }
+
+    while( glfwGetKey( GLFW_KEY_ESC ) != GLFW_PRESS &&
+           glfwGetWindowParam( GLFW_OPENED ) );
+
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+
+    exit( EXIT_SUCCESS );
+
+}
